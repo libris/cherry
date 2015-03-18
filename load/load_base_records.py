15712e13ab6dca7c1a07a3ac8ea82d1e1552a3b3
mskold <markus.skold@kb.se>
2015-03-18 13:09:28 +0100

    handle running collector multiple times

--- load/load_base_records.py
+++ load/load_base_records.py
@@ -3,13 +3,14 @@
 
 import json
 import requests
+import itertools
 from slugify import slugify
 
 esurl = "http://localhost:9200"
 
 
 def load_records():
-    url = "http://hp01.libris.kb.se:9200/libris_index/bib/_search?pretty"
+    url = "http://localhost:9200/libris_index/bib/_search?pretty"
     query = {
         "_source": [
             "about.@id",
@@ -23,40 +24,31 @@ def load_records():
         "query": {
             "term" : { "about.identifier.identifierScheme.@id" : "/def/identifiers/isbn" },
         },
-
         "filter" : {
-            "exists" : { "field" : ["about.creator", "about.title"] }
+            "exists" : { "field" : ["about.creator", "about.title" ] }
         }
-
     }
     ret = requests.post(url, data=json.dumps(query))
-    docs = []
+    docs = {}
     jres = json.loads(ret.text)
     for hit in jres['hits']['hits']:
         xl_record = hit.get('_source').get('about', {})
-        xl_identifier = xl_record.pop("@id")
 
         creator = xl_record.get('creator', [{}])[0]
         name = "{0} {1}".format(creator.get('givenName', ''), creator.get('familyName', '')) if creator.get('familyName') else "{0}".format(creator.get('name', ''))
-        #birthYear = creator.get("birthYear", "")
-        #slug_name = slugify("{0} {1}".format(name, birthYear), to_lower=True, separator='')
-        # Adding year to identifier breeds possibility for duplicate records.
         slug_name = slugify(name, to_lower=True, separator='')
 
         slug_title = slugify(xl_record.get('title', ''), to_lower=True, separator='')
 
         es_id = "{name}{title}".format(name=slug_name, title=slug_title)
 
-        cherry_record = load(es_id)
+        cherry_record = docs.get(es_id, load(es_id))
         cherry_record = combine_record(cherry_record, xl_record)
         cherry_record['@id'] = "/book/{name}/{title}".format(name=slug_name, title=slug_title)
-        cherry_record['derivedFrom'] = {"@id":xl_identifier}
 
-        docs += [ "{ \"index\" : { \"_id\" : \""+es_id+"\" }}" ]
-        docs += [ json.dumps(cherry_record) ]
+        docs[es_id] = cherry_record
 
     bulk_store(docs)
-
 
     # Poppa popcorn
     # Vin
@@ -65,14 +57,27 @@ def load_records():
 
 # TODO: check which fields should be overwritten
 def combine_record(old, new):
-    isbn = old.get('isbn',[])
+    xl_identifier = new.pop("@id")
+    isbn = set([])
+    if 'isbn' in old:
+        isbn = set(old['isbn'])
+    isbn.update(new.get('isbn', []))
+    isbn = list(filter(None, isbn))
     old.update(new)
-    old['isbn'] += isbn
+    if isbn:
+        old['isbn'] = isbn
+    if 'derivedFrom' in old:
+        derives = set([der['@id'] for der in itertools.chain([old['derivedFrom']])])
+        derives.add(xl_identifier)
+        old["derivedFrom"] = [{"@id":xlid} for xlid in derives]
+    else:
+        old["derivedFrom"] = {"@id":xl_identifier}
     return old
 
 def bulk_store(docs):
     print("Saving to ES")
-    r = requests.put(esurl + '/cherry/book/_bulk', data='\n'.join(docs)+'\n')
+    bulkdata = ["{ \"index\" : { \"_id\" : \""+es_id+"\" }}\n"+json.dumps(cherry_record) for (es_id, cherry_record) in docs.items()]
+    r = requests.put(esurl + '/cherry/book/_bulk', data='\n'.join(bulkdata)+'\n')
     print("Result of bulk:", r)
 
 def load(id):
