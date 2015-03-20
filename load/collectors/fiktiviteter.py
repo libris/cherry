4f6f15a51383cb2f52b30476d4edbdd21be21f01
mskold <markus.skold@kb.se>
2015-03-20 15:58:37 +0100

    fixed page parameter

--- load/collectors/fiktiviteter.py
+++ load/collectors/fiktiviteter.py
@@ -4,4 +4,4 @@
 from .blog import *
 
 if __name__ == "__main__":
-    consume("http://www.fiktiviteter.se/feed/atom/?page={page}", {'Accept': 'application/atom+xml'})
+    consume("http://www.fiktiviteter.se/feed/atom/?paged={page}", {'Accept': 'application/atom+xml'})
