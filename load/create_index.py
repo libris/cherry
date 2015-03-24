50b7459f37ef28e84da18fb660324edccf1a7c84
mskold <markus.skold@kb.se>
2015-03-24 06:27:28 +0100

    put all settings and mappings in one json file

--- load/create_index.py
+++ load/create_index.py
@@ -4,54 +4,29 @@
 import sys
 import argparse
 import json
-import pprint
-from index_config import create_config
 import requests
 
 def create(**args):
 
-
     base_url = 'http://localhost:9200'
 
-    # check if index exists
-    # Example: http://localhost:9200/ebok
-    #url = 'http://%s:%d/%s/' % (args['server'], args['port'], args['index'])
-    #url_c = 'http://localhost:9200/cherry/_close'# tunnel
-    #url_o = 'http://localhost:9200/cherry/_open'# tunnel
-    #url_m = 'http://localhost:9200/cherry/_mapping/{type}'# tunnel
-    #url_s = 'http://localhost:9200/cherry/_settings'# tunnel
     url = base_url + '/' + args['index']
-    #url = 'http://hp01.libris.kb.se:9200/cherry/'
-    #print("the url", url_m)
 
-    if 0: #requests.get(url + '_stats').status_code == 200:
+    if requests.get(url + '/_stats').status_code == 200:
         if args['delete']:
             print('Attempting delete of existing index at %s ...' % url)
             r = requests.delete(url)
-            print("\n{0} done: {1}".format(args['index'], r.text))
+            print("\ndelete {0} done: {1}".format(args['index'], r.text))
         else:
             print("Index exists, use --delete flag to force deletion before creation")
             exit(1)
-    
+
     # create index
-    ret = requests.put(url, data=json.dumps(create_config(args['index'], 'settings')))
+    cf = open('load/cherry_es_config.json','r')
+    ret = requests.put(url, data=cf.read())
 
-    print("create return ", )
-    for i in (json.loads(ret.text)).get('items', []):
-        print(i)
+    print("{0} create result: {1}".format(args['index'], ret.text))
 
-
-    if ret.status_code == 200:
-        print("settings succeeded for", url)
-        for etype,mapping in create_config(args['index'], 'mappings').items():
-            print("setting mappings for",etype)
-            ret = requests.put("{url}/_mapping/{type}".format(url=url, type=etype), data=json.dumps(mapping))
-            if ret.status_code == 200:
-                print("mappings succeeded for", etype)
-            else:
-                print("Could not map index, HTTP status %d\n%s" % (ret.status_code, ret.text))
-    else:
-        print("Could not create index, HTTP status %d\n%s" % (ret.status_code, ret.text))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Sets up an elasticsearch index for cherry project') 
