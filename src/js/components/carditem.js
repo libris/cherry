5f5214a76618a0e29d7fb2f584157f444eacaa69
Ola Blissing <ola.blissing@gmail.com>
2015-05-05 10:51:54 +0200

    Basic layout with masonry

---
+++ src/js/components/carditem.js
@@ -0,0 +1,16 @@
+var React = require('react')
+var ImageComponent = require('ainojs-react-image')
+
+
+module.exports = React.createClass({
+  render: function() {
+    return (
+        <div className="cardItem">
+        	<ImageComponent src="http://galleria.io/static/i/s2013/2m.jpg" lazy={true} ratio={114/200} />
+        	<div className="information">
+        		{this.props.data.get('title')}
+        	</div>
+        </div>
+    )
+  }
+})
