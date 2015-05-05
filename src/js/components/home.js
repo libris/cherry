f82983f88982cd973f1e203698c090bbdc135c5d
David Hellsing <david@aino.se>
2015-05-05 16:23:18 +0200

    masonry

--- src/js/components/home.js
+++ src/js/components/home.js
@@ -2,13 +2,10 @@ var React = require('react')
 var Data = require('../data')
 var trending = require('../collections').get('trending')
 var CardItem = require('./carditem')
-var MasonryMixin = require('react-masonry-mixin')
+var Masonry = require('../masonry')
 var $ = require('jquery')
 var Tick = require('next-tick')
 
-var masonryOptions = {
-    transitionDuration: 0
-};
 
 module.exports = React.createClass({
   displayName: 'HomeComponent',
@@ -27,16 +24,7 @@ module.exports = React.createClass({
     this.setState({
       loading: false
     }, function() {
-      if ( this.masonry )
-        return
-      var mix = MasonryMixin('masonryContainer', masonryOptions)
-      for( var prop in mix ) {
-        if ( !this.hasOwnProperty(prop) )
-          this[prop] = mix[prop]
-      }
-      this.initializeMasonry()
-      this.performLayout()
-      Tick(function() {
+      this.masonry.init(this.refs.container.getDOMNode(), function() {
         var evt = document.createEvent('MouseEvent')
         evt.initMouseEvent('scroll', true, true)
         window.dispatchEvent(evt)
@@ -45,9 +33,12 @@ module.exports = React.createClass({
   },
   componentWillUnmount: function() {
     // Destroy masonry
+    this.masonry.destroy()
   },
   componentDidMount: function() {
-
+    this.masonry = new Masonry({
+      transitionDuration: 0
+    })
   },
   render: function() {
     if ( this.state.loading ) {
@@ -56,6 +47,6 @@ module.exports = React.createClass({
     var trends = trending.map(function (trend) {
       return <CardItem key={trend.cid} data={trend} />
     })
-    return <div ref="masonryContainer">{trends}</div>
+    return <div ref="container">{trends}</div>
   }
 })
