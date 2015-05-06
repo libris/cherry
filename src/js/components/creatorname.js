e53b02957c185fc57bfbf78239714168421ee9df
Ola Blissing <ola.blissing@gmail.com>
2015-05-06 11:59:49 +0200

    creator name supports organisations

--- src/js/components/creatorname.js
+++ src/js/components/creatorname.js
@@ -2,7 +2,17 @@ var React = require('react')
 var _ = require('underscore')
 
 module.exports = React.createClass({
+
   render: function() {
+
+    var composeName = function (obj) {
+      if(obj['@type'] == 'Person'){
+        return obj.givenName + " " + obj.familyName;
+      } else if(obj['@type'] == 'Organization'){
+        return obj.name;
+      }
+    }
+
     var creatorList = []
     var creator = this.props.creator
 
@@ -10,10 +20,10 @@ module.exports = React.createClass({
 
     if(_.isArray(creator)){
       _.each(creator, function (item) {
-        creatorList.push(item.givenName + " " + item.familyName)
+        creatorList.push(composeName(item))
       })
     } else {
-      creatorList.push(creator.givenName + " " + creator.familyName)
+      creatorList.push(composeName(creator))
     }
 
     return (
