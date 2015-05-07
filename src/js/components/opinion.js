2012768d1c4679b063ce26ae53b1375a1178e24d
Ola Blissing <ola.blissing@gmail.com>
2015-05-07 17:10:03 +0200

    Opinion

--- src/js/components/opinion.js
+++ src/js/components/opinion.js
@@ -7,7 +7,10 @@ module.exports = React.createClass({
     return (
       <div className="opinion">
         <h2>BloggerDude83 <i className="fa fa-external-link-square"></i></h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div className="opinionText folded">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <button className="btn readMore">Läs hela</button>
+        </div>
       </div>
     )
   }
