14364bbbc35bb6fa2fd837c71f2aa3cc0f4b09b7
lisaw <lisa.winberg@kb.se>
2015-05-13 10:58:07 +0200

    has_child search in flt, aggregation not finished

--- cherry.py
+++ cherry.py
@@ -226,6 +226,7 @@ def api_flt():
         excluded_ids.append(ident)
 
     result = do_flt_query(request.args)
+    return json.dumps(result)
     #return json.dumps(result)
     qmeta = {"executed":query, "relatedPhrases":get_related_phrases_from_query_result(result, query)}
 
@@ -646,20 +647,44 @@ def do_flt_query(args, index_name=app.config['CHERRY']):
         "sort" : [],
         "size" : size,
 
-        "query" :  {"flt": {
-            "fields": ["text"],
-            "like_text": q,
-            "max_query_terms": 52,
-            "prefix_length": 4
-        }},
+    #    "query" :  {"flt": {
+    #        "fields": ["text"],
+    #        "like_text": q,
+    #        "max_query_terms": 52,
+    #        "prefix_length": 4
+    #    }},
 
+        "query": {
+            "has_child" : {
+                "type" : "annotation",
+                    "query" :  {"flt": {
+                        "fields": ["text"],
+                        "like_text": q,
+                        "max_query_terms": 52,
+                        "prefix_length": 4
+                    }},
+            }
+        },
         "fields": ["_parent","_source"],
 
-        "aggs" : {
-            #"unigrams" : {"significant_terms" : {"field" : "text.unigrams", "size": 30, "gnd": {}}},
-            "bigrams" : {"significant_terms" : {"field" : "text.bigrams", "size": 30, "gnd": {}}},
-            #"bigrams_gnd" : {"significant_terms" : {"field" : "text.shingles", "size": 10}},
+      #  "aggs" : {
+      #      #"unigrams" : {"significant_terms" : {"field" : "text.unigrams", "size": 30, "gnd": {}}},
+      #      "bigrams" : {"significant_terms" : {"field" : "text.bigrams", "size": 30, "gnd": {}}},
+      #      #"bigrams_gnd" : {"significant_terms" : {"field" : "text.shingles", "size": 10}},
+      #  },
+
+        "aggs": {
+            "parent_id": {
+                "terms": { 
+                    "field": "_parent",
+                    "size": 50
+                },
+                "aggs": {
+                    "bigrams" : {"significant_terms" : {"field" : "text", "size": 30, "gnd": {}}},
+                }
+            }
         }
+
     }
     if t:
         query['query'] = {
@@ -696,87 +721,6 @@ def do_flt_query(args, index_name=app.config['CHERRY']):
     r = es.search(body=query, index=index_name, doc_type=doctype)
     return r
 
-
-def old_do_flt_query(args, index_name=app.config['CHERRY']):
-    """Will search annotations if no other doctype is given."""
-    size=args.get('size')
-    q = args.get('q')
-    i = args.get('i')
-    doctype = args.get('doctype')
-    frm = args.get('frm')
-    to= args.get('to')
-    sort= args.get('sort')
-    t = args.get('t')
-    n = args.get('n')
-    f = args.get('f')
-    page = args.get('page')
-
-    if not doctype:
-        doctype=['annotation']
-    n = 50
-    date_filter = []
-    precision = 'y'
-
-    if i:
-        try:
-            q = child_texts(index_name, i)
-        except:
-            print("no record with id: ", i)
-            return {"err": 1, "msg": "Ingen post med angivet id"}
-
-    query = {
-        "sort" : [],
-        "size" : size,
-
-        "query" :  {"flt": {
-            "fields": ["text"],
-            "like_text": q,
-            "max_query_terms": 52,
-            "prefix_length": 4
-        }},
-
-        "fields": ["_parent","_source"],
-
-        "aggs" : {
-            #"unigrams" : {"significant_terms" : {"field" : "text.unigrams", "size": 30, "gnd": {}}},
-            "bigrams" : {"significant_terms" : {"field" : "text.bigrams", "size": 30, "gnd": {}}},
-            #"bigrams_gnd" : {"significant_terms" : {"field" : "text.shingles", "size": 10}},
-        }
-    }
-    if t:
-        query['query'] = {
-            "filtered": {
-                "query": {
-                    "flt": {
-                        "fields": ["text"],
-                        "like_text": q,
-                        "max_query_terms": 52,
-                        "prefix_length": 4
-                    }
-                },
-                "filter": {
-                    "and": [{ "isPartOf.@type": { "value": t}},]
-                }
-            }
-        }
-
-    if n:
-        n = int(n)
-        query['size'] = str(n)
-
-    if page:
-        print("page")
-        query['from'] = n * int(page)
-        print("the from", query['from'])
-
-
-
-
-    if f:
-        query['_source'] = f.split(',')
-
-    r = es.search(body=query, index=index_name, doc_type=doctype)
-    return r
 
 
 if __name__ == '__main__':
